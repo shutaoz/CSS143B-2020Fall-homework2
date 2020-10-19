66b67abd81a905096760af23af4eff495d7efc2f
shutaozhang <shutao76@uw.edu>
2020-10-18 22:13:13 -0700

    Version0 final0

--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -7,13 +7,20 @@ public class Problem2Test {
     public void testRemoveDuplicates() {
         int[][] inputs = {
                 // DO NOT REMOVE OR CHANGE THE GIVEN TEST CASES!
-                {0, 0, 1, 1, 1, 2, 2, 4, 4, 6}
+                {0, 0, 1, 1, 1, 2, 2, 4, 4, 6},
                 // add more test cases
+                {0, 1, 1, 1, 1, 2, 3, 3, 4, 6},
+                {0, 1, 1, 2, 3, 4, 5, 6},
+                {0, 0, 1, 1, 1, 1, 1, 1, 1, 1}
+
         };
 
         int[][] expects = {
-                {0, 1, 2, 4, 6}
+                {0, 1, 2, 4, 6},
                 // add more test cases
+                {0, 1, 2, 3, 4, 6},
+                {0, 1, 2, 3, 4, 5, 6},
+                {0, 1}
         };
 
         assertEquals(expects.length, inputs.length);
